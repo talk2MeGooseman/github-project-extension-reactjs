6e3eb9bc68f724c83b1010082d70dbee08205bf3
Erik Guzman <me.erik@me.com>
2017-12-20 22:39:20 -0800

    Building panel UI

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+import Viewer from "./views/Viewer";
+
+if (window.entryPoint === "VIEWER") {
+    ReactDOM.render(<Viewer />, document.getElementById("root"));
+} else if (window.entryPoint === "CONFIG") {
+    // ReactDOM.render(<Config />, document.getElementById("root"));
+} else if (window.entryPoint === "LIVE_VIEWER") {
+    // ReactDOM.render(<Viewer />, document.getElementById("root"));
+}
